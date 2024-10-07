e1b1ade35aa9368f6af9fb4e8caf40cd758f7db2
Brandon Leon Cabrera <230353@utxicotepec.edu.mx>
2024-10-07 11:54:28 -0600

    practica07
    comienzo del ejercicio_05

---
+++ JS/Ejercicio_05.js
@@ -0,0 +1,11 @@
+//Estilización de los mensajes de salida
+const bg = "linear-gradient(11deg, rgba(199,0,36,1) 0%, rgba(255,129,26,1) 33%, rgba(255,191,26,1) 86%)";
+const style_console = `background: ${bg}; color: white; border-radius: 6px; padding: 4px; font-size: 1.0rem; font-weight: bold`
+
+//Personalización de las Salidas a Consola
+console.warn("Prática 07: Arreglos en Java Script")
+
+console.log("%c1.- Sí/Entonces ... (IF)", style_console);
+//Le indica al programa que hacer o que no en base a una prueba lógica de verdadero o falso
+let fechaActual = new Date();
+console.log
